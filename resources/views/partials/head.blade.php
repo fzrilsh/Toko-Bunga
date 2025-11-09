b8865fd0cbb609aaa0abfa3207dc2bcf870573d3
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-09 08:14:35 +0700

    feat: add SEO metadata and structured data for improved search visibility

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,143 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+
+@if (request()->routeIs('dashboard.index') || request()->routeIs('pages.index') || !($product ?? $page ?? null))
+    <title>Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas</title>
+    <meta name="description" content="Cari toko bunga di Tangerang? Damai Agung Florist menyediakan bunga segar untuk berbagai acara. Hubungi kami sekarang!">
+    <meta name="keywords" content="toko bunga, toko bunga di tangerang, toko bunga di tangerang, bunga tangerang, damai, damai agung, Damai Agung Florist">
+    <link rel="sitemap" title="Sitemap" href="{{ asset('sitemap.xml') }}" type="application/xml">
+    <link rel="canonical" href="https://www.damaiagungflorist.com/">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas">
+    <meta property="og:description" content="Cari toko bunga di Tangerang? Damai Agung Florist menyediakan bunga segar untuk berbagai acara. Hubungi kami sekarang!">
+    <meta property="og:url" content="https://www.damaiagungflorist.com/">
+    <meta property="og:site_name" content="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas">
+    <meta property="article:modified_time" content="2024-01-10T15:28:37+00:00">
+    <meta property="og:image" content="{{ asset('imgs/medsos-image.jpeg') }}">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas">
+    <meta name="twitter:description" content="Cari toko bunga di Tangerang? Damai Agung Florist menyediakan bunga segar untuk berbagai acara. Hubungi kami sekarang!">
+    <meta name="twitter:image" content="{{ asset('imgs/medsos-image.jpeg') }}">
+
+    <link rel="alternate" type="application/rss+xml"
+        title="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas » Tentang Kami"
+        href="{{ route('pages.show', ['page' => 'tentang-kami']) }}">
+    <link rel="alternate" type="application/rss+xml"
+        title="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas  » Lokasi Kami"
+        href="https://www.google.com/maps/place/6°10'55.5%22S+106°38'34.2%22E/@-6.181651,106.64028,471m/data=!3m1!1e3!4m4!3m3!8m2!3d-6.1820833!4d106.6428333?hl=id&entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D">
+
+    <link rel="shortcut icon" href="{{ asset('imgs/logo.jpg') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('imgs/logo.jpg') }}">
+    <link rel="icon" sizes="192x192" href="{{ asset('imgs/logo.jpg') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('imgs/medsos-image.jpeg') }}">
+
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas">
+    <meta property="og:title" content="Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas">
+    <meta property="og:description"
+        content="Buket Bunga    
+    Bunga Meja      
+    Buket Bunga    
+    Buka Standing         
+    Rangkaian Bunga Terbaik 
+    100% Bunga Segar - 100% Pengiriman Cepat       
+    Produk Terbaru          Buket Bunga">
+    <meta property="og:url" content="https://www.damaiagungflorist.com/">
+    <meta property="og:image" content="{{ asset('imgs/medsos-image.jpeg') }}">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
+    <meta property="og:image:type" content="image/jpeg">
+
+    <script type="application/ld+json">
+        [
+            {
+                "@context": "https://schema.org",
+                "@type": "WebSite",
+                "@id": "https://www.damaiagungflorist.com#website",
+                "name": "Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas",
+                "headline": "Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas",
+                "description": "Cari toko bunga di Tangerang? Damai Agung Florist menyediakan bunga segar untuk berbagai acara. Hubungi kami sekarang!",
+                "url": "https://www.damaiagungflorist.com",
+                "potentialAction": {
+                    "@type": "SearchAction",
+                    "target": "https://www.damaiagungflorist.com/products?search={search_term_string}",
+                    "query-input": "required name=search_term_string"
+                }
+            },
+            {
+                "@context": "https://schema.org",
+                "@type": "LocalBusiness",
+                "@id": "https://www.damaiagungflorist.com#LocalBusiness",
+                "url": "https://www.damaiagungflorist.com",
+                "name": "Damai Agung Florist",
+                "description": "Cari toko bunga di Tangerang? Damai Agung Florist menyediakan bunga segar untuk berbagai acara. Hubungi kami sekarang!",
+                "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "Jl. Eksekusi 2 Blok E2 No.8, RT.001/RW.008, Sukasari, Kec. Tangerang",
+                    "addressLocality": "Kota Tangerang",
+                    "addressRegion": "Banten",
+                    "postalCode": "15118",
+                    "addressCountry": "ID"
+                },
+                "telephone": ["+6283813735071", "+6282310498541"],
+                "openingHours": ["24/7"],
+                "image": "https://www.damaiagungflorist.com/imgs/logo.jpg",
+                "priceRange": "IDR 20.000 - IDR 8.500.000",
+                "founder": {
+                    "@type": "Person",
+                    "name": "Anita Putri Lestari"
+                },
+                "sameAs": [
+                    "https://www.facebook.com/damaiagungflorist/",
+                    "https://www.instagram.com/damaiagungflorist/"
+                ],
+                "aggregateRating": {
+                    "@type": "AggregateRating",
+                    "ratingValue": "4.9",
+                    "reviewCount": "3776"
+                }
+            },
+            {
+                "@context": "https://schema.org",
+                "@type": "Corporation",
+                "@id": "https://www.damaiagungflorist.com#Organization",
+                "name": "Toko Bunga di Tangerang - Harga Terjangkau Produk Kerkualitas",
+                "url": "https://www.damaiagungflorist.com",
+                "sameAs": [
+                    "https://www.facebook.com/damaiagungflorist/",
+                    "https://www.instagram.com/damaiagungflorist/"
+                ],
+                "logo": {
+                    "@type": "ImageObject",
+                    "url": "https://www.damaiagungflorist.com/imgs/logo.jpg",
+                    "width": 200,
+                    "height": 200
+                },
+                "contactPoint": {
+                    "@type": "ContactPoint",
+                    "contactType": "sales",
+                    "telephone": ["+6283813735071", "+6282310498541"],
+                    "url": "https://www.damaiagungflorist.com/kontak-kami/"
+                }
+            }
+        ]
+    </script>
+@else
+    {!! seo($product ?? $page ?? null) !!}
+@endif
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Lora:ital,wght@0,400..700;1,400..700&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+@vite('resources/css/app.css')
+<link rel="stylesheet" href="{{ asset('css/animations.css') }}" />
+
+@stack('styling')
